30aa5780e79b6e804e3bbf3ead602290ab9d7e3c
Stas74 <splotnikov74@gmail.com>
2022-02-01 20:34:28 -0700

    Add characters counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,4 +3,10 @@ console.log('Cheeeck!!!!');
 $(document).ready(function() {
   // --- our code goes here ---
   console.log('Cheeeck!!!!');
+  let charCount = 0;
+  $("#tweet-text").on("input", function(){
+    charCount = document.getElementById("tweet-text").value.length;
+    console.log("charCount", charCount);   
+  });
+  
 });
