c0268cd32e60448bbc4e309b9737fb9a7545302b
Stas74 <splotnikov74@gmail.com>
2022-02-01 21:21:44 -0700

    Add functional counter and changing color, if counter less then 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,17 @@
-console.log('Cheeeck!!!!');
-
 $(document).ready(function() {
   // --- our code goes here ---
   console.log('Cheeeck!!!!');
   let charCount = 0;
+  let leftCounter;
   $("#tweet-text").on("input", function(){
     charCount = document.getElementById("tweet-text").value.length;
-    console.log("charCount", charCount);   
-  });
-  
+    leftCounter = 140 - charCount;
+    console.log("charCount", leftCounter); 
+    document.getElementById("counter").innerHTML = leftCounter;
+    if (leftCounter < 0) {
+      $("#counter").css('color', 'red');      
+    } else {
+      $("#counter").css('color', '#545149');
+    }
+  });  
 });
