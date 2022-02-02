79e6df9ad7cf895644b7e003fa2fce08192d356d
Stas74 <splotnikov74@gmail.com>
2022-02-02 15:24:51 -0700

    comment out (wrong part)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// $(document).ready(function() {
+//   console.log('Client.js check');
+//   $.getJSON("/server/data-files/initial-tweets.json", function(data){
+//     console.log("data", data)
+//   }) 
+
+// });
