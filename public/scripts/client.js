e7d635749cfbc819ff2bafb7c5881e912a3868c6
Stas74 <splotnikov74@gmail.com>
2022-02-03 23:03:07 -0700

    Remove all comment out code, console.log and final version of fine tune

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,135 +4,86 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-
 $(() => {
 
   $("#error").hide();
- 
-  const escape = function(str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
 
   const error = (message) => {
     $("#error").text(message);
     $("#error").show();
   };
 
-
   const renderTweets = function(tweets) {
-    // loops through tweets
-    // $('#container-tweets').empty();
-    console.log("tweets", tweets);
     for (const tweet of tweets) {
-      console.log("tweet", tweet);
-      // calls createTweetElement for each tweet
       let $tweetElement = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $('#container-tweets').prepend($tweetElement); // move last tweet on top
+      $('#container-tweets').prepend($tweetElement); // move sended tweet on top
     }
-    // takes return value and appends it to the tweets container
   };
-
-  const createTweetElement = (tweet) => {
-    console.log("inside function");
-    let $tweet = $(`<article id="article">
-    <header>
-      <div class="tweet-header">
-        <div class="avatar">
-        <img width="70px" height="70px" alt="profile img" src="${tweet.user.avatars}"></img>
-        <p>${tweet.user.name}</p>
-        </div>
-      <div>                
-        <p>${tweet.user.handle}</p>
-      </div>
-    </header>
-    ${tweet.content.text}
-    <footer class="tweet-footer">                
-      <div class="timeago">
-        <p>${timeago.format(tweet.created_at)}</p>
-      </div>
-      <div class="footer-icons">
-        <p><i class="fa-solid fa-flag"></i></p>
-        <p><i class="fa-solid fa-retweet"></i></p>
-        <p><i class="fa-solid fa-heart"></i></p>
-      </div>
-    </footer>
-   </article>`);
-      
-    return $tweet;
-  };
-
-  $('#add-tweet').on('submit', function(event) {
-    // prevent the default behaviour of the form (making a GET request to the current page)
-    console.log('form has submitted');
-    event.preventDefault();
-    $("#error").hide(); // hide error
-  
-    // console.log($(this).serialize());
-
-    //const safeHTML = `<p>${escape(this)}</p>`;
-    $("<div>").text(this); // other check text from user
-
-    const data = $(this).serialize();
-    
-
-    // console.log('data', data);
-    // console.log("length", typeof data)
-    
-    const counter = $("#counter");
-    console.log("counter",counter);
-
-    if (data === "text=") {
-      error("Can not be empty!");
-      return;
-    } else if (counter.val() < 0) {
-      error("Tweet is too long!");
-      // return;
-    } else {
-      $.ajax({
-        method: 'POST',
-        url: '/tweets',
-        data: data
-      }).then((success) => {
-        console.log("Hello", success);
-  
-        $('#tweet-text').val('');
-        counter.val('140');
-        loadTweets();
-      });
-    }
-
-    
-  });
-  
 
   const loadTweets = () => {
     $.ajax({
       url: '/tweets',
       method: 'GET'
     }).then((success) => {
-      // $('#container-tweets').empty();
       renderTweets(success);
     });
   };
+  
+
+  const createTweetElement = (tweet) => {
+    let $tweet = $(
+      `<article id="article">
+        <header>
+          <div class="tweet-header">
+            <div class="avatar">
+            <img width="70px" height="70px" alt="profile img" src="${tweet.user.avatars}"></img>
+            <p>${tweet.user.name}</p>
+            </div>
+          <div>                
+            <p>${tweet.user.handle}</p>
+          </div>
+        </header>
+        ${tweet.content.text}
+        <footer class="tweet-footer">                
+          <div class="timeago">
+            <p>${timeago.format(tweet.created_at)}</p>
+          </div>
+          <div class="footer-icons">
+            <p><i class="fa-solid fa-flag"></i></p>
+            <p><i class="fa-solid fa-retweet"></i></p>
+            <p><i class="fa-solid fa-heart"></i></p>
+          </div>
+        </footer>
+      </article>`
+    );
+    return $tweet;
+  };
+
+  $('#add-tweet').on('submit', function(event) {
+    event.preventDefault();
+    $("#error").hide(); // hide error
+
+    $("<div>").text(this); // check text from user
+
+    const data = $(this).serialize();
+    
+    const counter = $("#counter");
+
+    if (data === "text=") {
+      error("Sorry! Can not be empty!");      
+    } else if (counter.val() < 0) {
+      error("Tweet is too long!");      
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: data
+      }).then(() => {
+        $('#tweet-text').val('');
+        counter.val('140');
+        loadTweets();
+      });
+    }
+  });
   loadTweets();
 });
-
-
-
-
-// $(document).ready(function() {
-//   console.log('Client.js check');
-//   $.getJSON("/server/data-files/initial-tweets.json", function(data){
-//     console.log("data", data)
-//   })
-
-// });
-
-// const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-// Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#container-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log('Check composer.js');
+  
   let charCount = 0;
   let leftCounter;
-  $("#tweet-text").on("input", function(){
+  $("#tweet-text").on("input", function() {
     charCount = document.getElementById("tweet-text").value.length;
+    $(this).height(0);
+    $(this).height(this.scrollHeight);
     leftCounter = 140 - charCount;
-    console.log("charCount", leftCounter); 
     document.getElementById("counter").innerHTML = leftCounter;
     if (leftCounter < 0) {
-      $("#counter").css('color', 'red');      
+      $("#counter").css('color', 'red');
     } else {
       $("#counter").css('color', '#545149');
     }
-  });  
+  });
 });
